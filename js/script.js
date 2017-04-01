b1abe17e7c7461b1bde418e9070143f4b2d055c8
aswinzz <iit2016106@iiita.ac.in>
2017-04-01 22:19:04 +0530

    Added Initial Stage

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+var channelName='TechGuyWeb';
+$(document).ready(function(){
+	
+	 	$.get(
+           "https://www.googleapis.com/youtube/v3/channels",{
+           	part: 'contentDetails',
+           	forUsername: channelName,
+           	key: 'AIzaSyBPxaszELtUXG5JuuJOQWioZ0g7uNaCiEE'
+           },
+           function(data){
+           	$.each(data.items,function(i,item){
+           		console.log(item);
+           		pid = item.contentDetails.relatedPlaylists.uploads;
+           		getVids(pid);
+           	})
+           }
+		);
+		function getVids(pid){
+				$.get(
+           "https://www.googleapis.com/youtube/v3/playlistItems",{
+           	part: 'snippet',
+           	maxResults: 10,
+           	playlistId: pid,
+           	key: 'AIzaSyBPxaszELtUXG5JuuJOQWioZ0g7uNaCiEE'
+           },
+           function(data){
+           	var output
+           	$.each(data.items,function(i,item){
+           		console.log(item);
+           		videTitle=item.snippet.title;
+           		videoId=item.snippet.resourceId.vedioId;
+           		output='<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>'
+           		//Append to Results
+           		$('#results').append(output); 
+           	})
+           }
+		);
+		}
+});
